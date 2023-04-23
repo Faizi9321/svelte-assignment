8dfacd16177ee8061d0a17e15083d244c10af02f
FaizanAwais <111850172+FaizanAwais@users.noreply.github.com>
2023-04-23 12:02:40 +0500

    fixed define points...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
 	import SveltyPicker from 'svelty-picker';
-	import { Button } from '@svelteuidev/core';
+	import { Button, Text } from '@svelteuidev/core';
 	import { Plus, Cross1 } from 'radix-icons-svelte';
 	import { Container } from '@svelteuidev/core';
     import '../style.scss'
 
 	let days: any[] = [
-		{ day: 'Monday', timeSlots: [{ openTime: '', closeTime: '' ,btnDisable:true}] },
-		{ day: 'Tuesday', timeSlots: [{ openTime: '', closeTime: '' ,btnDisable:true}] },
-		{ day: 'Wednesday', timeSlots: [{ openTime: '', closeTime: '' ,btnDisable:true}] },
-		{ day: 'Thursday', timeSlots: [{ openTime: '', closeTime: '' ,btnDisable:true}] },
-		{ day: 'Friday', timeSlots: [{ openTime: '', closeTime: '' ,btnDisable:true}] },
-		{ day: 'Saturday', timeSlots: [{ openTime: '', closeTime: '' ,btnDisable:true}] },
-		{ day: 'Sunday', timeSlots: [{ openTime: '', closeTime: '' ,btnDisable:true}] }
+		{ day: 'Monday', timeSlots: [{ openTime: '', closeTime: '' }] },
+		{ day: 'Tuesday', timeSlots: [{ openTime: '', closeTime: '' }] },
+		{ day: 'Wednesday', timeSlots: [{ openTime: '', closeTime: '' }] },
+		{ day: 'Thursday', timeSlots: [{ openTime: '', closeTime: '' }] },
+		{ day: 'Friday', timeSlots: [{ openTime: '', closeTime: '' }] },
+		{ day: 'Saturday', timeSlots: [{ openTime: '', closeTime: '' }] },
+		{ day: 'Sunday', timeSlots: [{ openTime: '', closeTime: '' }] }
 	];
+
     const addMore = (item:string) => {
+        console.log(item);
         days.forEach((element)=>{
-            if(element.day == item){
-                element.timeSlots.push({ openTime: '', closeTime: '', btnDisable:false})
+            if(element.timeSlots[0] == 'Closed'){
+                console.log('close');
+                element.timeSlots = []
+                element.timeSlots.push({ openTime: '', closeTime: ''})
+            }else if(element.day == item){
+                element.timeSlots.push({ openTime: '', closeTime: ''})
             }
-            
         })
         days = days
     }
@@ -29,7 +34,9 @@
             if(element.day == item){
                 element.timeSlots.splice(index,1)
             }
-            
+            if(element.timeSlots.length == 0){
+                element.timeSlots.push('Closed')
+            }            
         })
         days = days
     }
@@ -37,37 +44,51 @@
 
 <Container override={{ dflex: 'center', fd: 'column', my: '80px' }}>
 	{#each days as item}
-		<Container override={{ dflex: 'center', mt: '10px' }}>
-			<Container override={{ mx: '30px', w: '100px', textAlign: 'center' }}>
-				{item.day}
+		<Container override={{ d: 'flex', jc:'center', ac:'start', mt: '10px' }}>
+			<Container override={{ mx: '10px',mt: '11px',w: '100px', textAlign: 'center' }}>
+				 <Text>{item.day}</Text>
 			</Container>
-			<Container override={{d:'flex',fw:'wrap',w:'350px',pr:'0px'}}>
-				{#each item.timeSlots as hours, index}
-                    <Container override={{mb:'8px'}}>
-                        <SveltyPicker
-                            placeholder="open"
-                            mode="time"
-                            format="hh:ii"
-                            bind:value={hours.openTime}
-                            inputClasses="inputTime"
-                        />
-                        <SveltyPicker
-                            placeholder="close"
-                            mode="time"
-                            format="hh:ii"
-                            bind:value={hours.closeTime}
-                            inputClasses="inputTime"
-                        />
+			<Container override={{d:'flex',fw:'wrap',w:'264px',pr:'0px'}}>
+                {#if item.timeSlots[0] == 'Closed'}
+                    <Container override={{px: '48px', py: '11px'}}>
+                        <Text>{item.timeSlots[0]}</Text>
                     </Container>
-                    <Container override={{ dflex: 'center' }}>
-                        <Button override={{mx:'6px'}} on:click="{() => addMore(item.day)}">
+                    <Container override={{ dflex: 'center',px: '0px' }}>
+                        <Button override={{mx:'6px',w: '40px',px: '12px'}} on:click="{() => addMore(item.day)}">
                             <Plus />
                         </Button>
-                        <Button override={{mx:'6px',bgColor:'Silver'}} on:click="{() => onRemove(item.day,index)}" disabled={hours.btnDisable}>
+                        <Button override={{mx:'6px',bgColor:'Silver',w: '40px',px: '12px'}} on:click="{() => onRemove(item.day,0)}" disabled={true}>
                             <Cross1 />
                         </Button>
                     </Container>
-				{/each}
+                {:else if item.timeSlots.length > 0}
+                    {#each item.timeSlots as hours, index}
+                        <Container override={{my:'4px',px: '0px',py: '0px'}}>
+                            <SveltyPicker
+                                placeholder="open"
+                                mode="time"
+                                format="hh:ii"
+                                bind:value={hours.openTime}
+                                inputClasses="inputTime"
+                            />
+                            <SveltyPicker
+                                placeholder="close"
+                                mode="time"
+                                format="hh:ii"
+                                bind:value={hours.closeTime}
+                                inputClasses="inputTime"
+                            />
+                        </Container>
+                        <Container override={{ dflex: 'center',px: '0px' }}>
+                            <Button override={{mx:'6px',w: '40px',px: '12px'}} on:click="{() => addMore(item.day)}">
+                                <Plus />
+                            </Button>
+                            <Button override={{mx:'6px',bgColor:'Silver',w: '40px',px: '12px'}} on:click="{() => onRemove(item.day,index)}">
+                                <Cross1 />
+                            </Button>
+                        </Container>
+                    {/each}
+                {/if}
 			</Container>
 		</Container>
 	{/each}
